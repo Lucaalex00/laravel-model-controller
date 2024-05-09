c684b26f2f2310a5bebd3165aec3b1b5ead23e6d
Lucaalex00 <lu.al.cirio00@gmail.com>
2024-05-09 15:28:55 +0200

    Add Movies from database laravel_model_controller printed Commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,6 +17,41 @@
 
 <body>
 
+    <div class="container p-3">
+        <div class="row row-cols-md-2 g-3">
+            @forelse ($movies as $movie)
+                <div class="col">
+                    <div class="card bg-light p-3 position-relative">
+                        <h6 class="position-absolute top-0 end-0"> {{ $movie->id }}.</h6>
+                        <div class="card_title">
+                            <h3>{{ $movie->title }}</h3>
+                            <h6>"{{ $movie->original_title }}"</h6>
+                        </div>
+                        <div class="card_body">
+                            <img src="https://picsum.photos/200" alt="">
+                            <div>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam ratione
+                                    ad optio quod
+                                    nulla delectus officiis nam adipisci, voluptatibus esse blanditiis dicta modi
+                                    deleniti voluptatem, libero non, debitis atque? Ipsam.Amet saepe neque molestias
+                                    vel? Debitis totam dolorum ea quam quis! Quidem dolor nihil repellendus ipsa
+                                    veritatis architecto, pariatur blanditiis sunt officia ea sequi unde accusamus
+                                    eligendi doloremque accusantium minus.
+                                </p>
+                                <strong>{{ $movie->nationality }}</strong>
+                            </div>
+                        </div>
+                        <div class="card_footer text-end">
+                            <div>Release date : <strong>{{ $movie->date }}</strong></div>
+                            <div>Vote : <strong>{{ $movie->vote }}</strong></div>
+                        </div>
+                    </div>
+                </div>
+            @empty
+                <h1>There aren't any movies</h1>
+            @endforelse
+        </div>
+    </div>
 </body>
 
 </html>
